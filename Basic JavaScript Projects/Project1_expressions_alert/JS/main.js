4d691d95480cb7f61c32424b8eb6fb6ec2cc6187
solicyrus <cyrusamiri99@gmail.com>
2025-08-26 16:50:57 -0700

    Add JavaScript project 1

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+
+var A = "Hello, This Is My JavaScript Assignment";
+window.alert(A)
+document.write("I told him, \"I'd kill to have a vacation right now\"<br>")
+document.write("\"Be who you are and say what you feel," + " because those who mind don\'t matter and those who matter don\'t mind.\"" + "-Dr. Seuss<br><br>");
+var B = "Hello World again!<br><br><br>" + "\"Be who you are and say what you feel," + " because those who mind don\'t matter and those who matter don\'t mind.\"" + "-Dr. Seuss<br><br>"
+document.write(B)
+document.write(B + A);
+var fruit = "my favorite", number1 ="Cherry", number2 ="Strawberry", number3 ="Apple", number4 ="Peach";
+document.write(number2)
+document.write(3+3);
+function My_First_Function(){//Defining a function and naming it
+    var str = "This is the button text!";//Defining a function and naming it
+    document.getElementById("Button_Text") .innerHTML = str; /*Putting the valy=ue
+                                              of the variable into the HTML elementFormPoint
+                                              with the "Button_Text" id*/
+}
